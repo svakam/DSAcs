aa134b2096337a6aa4673789d2065461a4245652
Vik A <vikramakam@hotmail.com>
2021-11-20 18:54:55 -0800

    renamed to recursive

--- DSAcs/DSAcs/Tree/Tree.cs
+++ DSAcs/DSAcs/Tree/Tree.cs
@@ -156,16 +156,16 @@ namespace DSAcs.Tree
         //    return count;
         //}
 
-        public int GetMaxDepth(Tree tree)
+        public int GetMaxDepthRecursive(Tree tree)
         {
-            return GetMaxDepth(tree.Root);
+            return GetMaxDepthRecursive(tree.Root);
         }
-        public int GetMaxDepth(TreeNode node)
+        public int GetMaxDepthRecursive(TreeNode node)
         {
             if (node == null) return -1; // when a node is null, returns as -1, but will return as 0 when adding its parent (-1 + 1)
 
-            int lDepth = GetMaxDepth(node.Left);
-            int rDepth = GetMaxDepth(node.Right);
+            int lDepth = GetMaxDepthRecursive(node.Left);
+            int rDepth = GetMaxDepthRecursive(node.Right);
 
             if (lDepth > rDepth)
             {
