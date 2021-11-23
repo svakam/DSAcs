671dbab3b0e063b7e006928d6ad5316a85514e99
Vik A <vikramakam@hotmail.com>
2021-11-23 12:51:43 -0800

    moved enums to new treebase superclass

---
+++ DSAcs/DSAcs/Tree/TreeBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAcs.Tree
+{
+    public class TreeBase
+    {
+        protected static TraversalType Traversal { get; set; }
+        protected static RemoveMethod RemovalMethod { get; set; }
+    }
+
+    public enum TraversalType
+    {
+        PREORDER,
+        INORDER,
+        POSTORDER,
+        BREADTHFIRST
+    }
+
+    public enum RemoveMethod
+    {
+        INORDERPRECURSOR,
+        INORDERSUCCESSOR
+    }
+}
