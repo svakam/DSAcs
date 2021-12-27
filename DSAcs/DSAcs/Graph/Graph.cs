22c20ff5637a6510ae7a152e59db24cb23bea167
Vik A <vikramakam@hotmail.com>
2021-12-27 14:23:53 -0800

    added graph methods

--- DSAcs/DSAcs/Graph/Graph.cs
+++ DSAcs/DSAcs/Graph/Graph.cs
@@ -12,24 +12,60 @@ namespace DSAcs.Graph
     {
         public LinkedListS<Vertex> Vertices { get; set; }
         public LinkedListS<Edge> EdgeList { get; set; }
-        public int[][] AdjacencyMatrix { get; set; }
+        public int[,] AdjacencyMatrix { get; set; }
         public LinkedListS<Edge> AdjacencyList { get; set; }
         public StorageUsed StorageUsed { get; set; }
 
-        public Graph()
+        public Graph() { }
+        public Graph(LinkedListS<object> vertices, StorageUsed storageUsed)
         {
-            if (StorageUsed == StorageUsed.EDGELIST)
+            // set up vertices: for each vertex, set up ll with vertex
+            if (Vertices != null)
             {
-
+                CleanGraph();
             }
-            else if (StorageUsed == StorageUsed.ADJACENCYMATRIX)
-            {
 
+            Vertices = new LinkedListS<Vertex>();
+            NodeS curr = (NodeS) vertices.Head;
+            int length = 1;
+            while (curr != null)
+            {
+                AddVertex(curr.Data);
+                curr = (NodeS) curr.Next;
+                length++;
+            }
+
+            if (storageUsed == StorageUsed.EDGELIST)
+            {
+                EdgeList = new LinkedListS<Edge>();
+            }
+            else if (StorageUsed == StorageUsed.ADJACENCYLIST)
+            {
+                AdjacencyList = new LinkedListS<Edge>();
             }
             else
             {
-
+                AdjacencyMatrix = new int[length, length];
             }
+        }
+
+        public void CleanGraph()
+        {
+            Vertices = null;
+            EdgeList = null;
+            AdjacencyList = null;
+            AdjacencyMatrix = null;
+            StorageUsed = StorageUsed.NONE;
+        }
+
+        public void AddVertex(object start)
+        {
+            if (Vertices == null)
+            {
+                Vertices = new();
+            }
+            Vertex v = new(start);
+            Vertices.Add(v);
         }
 
         public void DFS(object start)
@@ -45,12 +81,54 @@ namespace DSAcs.Graph
 
             throw new ArgumentException("Vertex does not exist in this graph.");
         }
+
+        private bool SearchForVertex(object a)
+        {
+            // search for vertex in the list
+            // if exists, mark as seen and return true
+            // else return false
+
+            NodeS curr = (NodeS) Vertices.Head;
+            while (curr != null)
+            {
+                Vertex currVertex = (Vertex) curr.Data;
+                if (currVertex.Data == a)
+                {
+                    return true;
+                }
+                curr = (NodeS) curr.Next;
+            }
+            return false;
+        }
+
+        public void AddEdge(object a, object b)
+        {
+            if (Vertices == null) throw new InvalidOperationException("Cannot add an edge on an empty graph.");
+
+            bool aExists = SearchForVertex(a);
+            bool bExists = SearchForVertex(b);
+            if (aExists && bExists)
+            {
+                if (StorageUsed == StorageUsed.EDGELIST)
+                {
+                    Edge e = new(a, b);
+                    EdgeList.Add(e);
+                }
+                // adjacency matrix
+                // adjacency list
+            }
+            else
+            {
+                throw new ArgumentException($"Cannot add edge. First parameter exists: {aExists}. Second parameter exists: {bExists}.");
+            }
+        }
     }
 
     public enum StorageUsed
     {
         EDGELIST,
         ADJACENCYMATRIX,
-        ADJACENCYLIST
+        ADJACENCYLIST,
+        NONE
     }
 }
