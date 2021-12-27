51bf1d09126a8f6470757fd511045f4dd0d03259
Vik A <vikramakam@hotmail.com>
2021-12-27 12:52:07 -0800

    added constructors to edge

--- DSAcs/DSAcs/Graph/Edge.cs
+++ DSAcs/DSAcs/Graph/Edge.cs
@@ -7,5 +7,18 @@ namespace DSAcs.Graph
         public Vertex Start { get; set; }
         public Vertex End { get; set; }
         public int Weight { get; set; }
+
+        public Edge(object start, object end)
+        {
+            Start = new Vertex(start);
+            End = new Vertex(end);
+        }
+
+        public Edge(object start, object end, int weight)
+        {
+            Start = new Vertex(start);
+            End = new Vertex(end);
+            Weight = weight;
+        }
     }
 }
