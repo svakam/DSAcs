f25e0edbec8cbaa69def2b9e9f945f41a87843cd
Vik A <vikramakam@hotmail.com>
2022-01-12 13:33:06 -0800

    added TrieNode class

---
+++ DSAcs/DSAcs/Nodes/TrieNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAcs.Nodes
+{
+    public class TrieNode
+    {
+        public Dictionary<char, TrieNode> Children { get; set; }
+        public bool EndOfWord { get; set; }
+
+        public TrieNode()
+        {
+            Children = new Dictionary<char, TrieNode>();
+            EndOfWord = false;
+        }
+    }
+}
